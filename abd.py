b62ac591c326b39d22749a29fe4184340c4f3f7c
abdulahshahid <abdullah.shahid1045@gmail.com>
2023-10-09 04:08:03 -0700

    upto this point i have created the basic laypout of my class

---
+++ abd.py
@@ -0,0 +1,18 @@
+# abdullah shahid
+# NIM-BSCS-2021-07
+
+"""
+in this file i am going to make
+a calculator
+"""
+
+class Calculator:
+
+    def __init__(self, n1, n2, operation):
+        self.n1 = n1
+        self.n2 = n2
+        self.operation = operation
+    def run(self):
+        pass
+    def choose_what_to_do():
+        pass
